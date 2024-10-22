ba94467af8b0dcf5e66eb460cb26ad6fa8646065
Md Kutub Uddin <mdkutub150@gmail.com>
2024-10-22 22:04:13 +0600

    add

--- Android Project With Firebase/Blood_And_Buddies/app/src/main/java/com/kutub/bloodandbuddies/Email/Util.java
+++
@@ -1,7 +0,0 @@
-package com.kutub.bloodandbuddies.Email;
-
-public class Util {
-
-    public static final String EMAIL = "developerofanderoid@gmail.com";
-    public static final String PASSWORD = "TU5HA12_developer";
-}
